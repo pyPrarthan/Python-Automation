3dc14d47867557593bd2bd708549c7084e3d3e4c
Prarthan Christian <156262672+pyPrarthan@users.noreply.github.com>
2024-08-16 18:24:15 -0400

    Delete tableExtraction.py

--- tableExtraction.py
+++
@@ -1,8 +0,0 @@
-#Extracting table  from URL 
-
-import pandas as pd
-
-strangerThings = pd.read_html('https://en.wikipedia.org/wiki/List_of_Stranger_Things_episodes')
-
-print(len(strangerThings))
-print(strangerThings[0])
