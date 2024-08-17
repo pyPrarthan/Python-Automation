88b3dabfb6461838a40b6951f3a70e43ee78d51c
pyPrarthan <christianprarthan07@gmail.com>
2024-08-16 20:45:19 -0400

    more logic added

--- 2.Automate The News/newsHeadline.py
+++ 2.Automate The News/newsHeadline.py
@@ -18,12 +18,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 website = "https://www.thesun.co.uk/sport/football/"
-
 path = r"C:\Users\anujc\Desktop\chromedriver-win64 (1)\chromedriver-win64/chromedriver.exe"
 
 service = Service(executable_path=path)
-
 driver = webdriver.Chrome(service=service)
-
-
 driver.get(website)
+
+containers = driver.find_elemenst(by="xpath", value='//div[@class="teaser__copy-container"]')
+
+for container in containers:
+    title = container.find_elemnt(by="xpath", value='./a/h3').text
+    subtitle = container.find_elemnt(by="xpath", value='./a/p').text
+    link = container.find_element(by='xpath', value='./a').get_attribute("href")
