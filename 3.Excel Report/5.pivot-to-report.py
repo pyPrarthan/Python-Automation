708e91bae849d0d6235a107bfc889e973f9a227f
pyPrarthan <christianprarthan07@gmail.com>
2024-08-21 14:28:03 -0400

    issue resolved

--- 3.Excel Report/5.pivot-to-report.py
+++ 3.Excel Report/5.pivot-to-report.py
@@ -2,13 +2,24 @@ from openpyxl import load_workbook
 from openpyxl.chart import BarChart, Reference
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Font, NamedStyle
+import os
+import sys
+
+# Get application path
+application_path = os.path.dirname(sys.executable)
 
 # Define month and read workbook
-month = 'february'
-wb = load_workbook('pivot_table.xlsx')
+month = input('Input Month: ')
+
+# Define input and output paths
+input_path = os.path.join(application_path, 'pivot_table.xlsx')
+output_path = os.path.join(application_path, f'report_{month}.xlsx')
+
+# Load workbook and select sheet
+wb = load_workbook(input_path)
 sheet = wb['Report']
 
-# Active rows and columns
+# Get active rows and columns
 min_column = sheet.min_column
 max_column = sheet.max_column
 min_row = sheet.min_row
@@ -21,11 +32,11 @@ barchart = BarChart()
 data = Reference(sheet, min_col=min_column + 1, max_col=max_column, min_row=min_row, max_row=max_row)  # including headers
 categories = Reference(sheet, min_col=min_column, max_col=min_column, min_row=min_row + 1, max_row=max_row)  # not including headers
 
-# Adding data and categories
+# Adding data and categories to the chart
 barchart.add_data(data, titles_from_data=True)
 barchart.set_categories(categories)
 
-# Make chart
+# Place the chart at a specific cell
 sheet.add_chart(barchart)
 barchart.title = 'Sales by Product line'
 barchart.style = 5  # choose the chart style
@@ -46,4 +57,5 @@ sheet['A2'] = month
 sheet['A1'].font = Font(name='Arial', bold=True, size=20)
 sheet['A2'].font = Font(name='Arial', bold=True, size=10)
 
-wb.save(f'report_{month}.xlsx')
+# Save the workbook
+wb.save(output_path)
